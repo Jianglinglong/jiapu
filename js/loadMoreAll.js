6d9643d5cb91d9a35c9f0581f59027daa0da32c5
jiang <2899297045@qq.com>
2018-06-14 20:26:18 +0800

    族谱搜索搜索

--- js/loadMoreAll.js
+++ js/loadMoreAll.js
@@ -1,5 +1,5 @@
 var isLoad = true;
-var islast =true;
+var islast = true;
 var page = 1;
 $(window).scroll(function () {
     if (isLoad || islast) {
@@ -10,24 +10,27 @@ $(window).scroll(function () {
     var windowHeight = $(this).height();
     if ((scrollTop + windowHeight == scrollHeight) && !islast) {
         isLoad = true;
-        loadMore();
+        loadMore(shousou);
     }
 });
+
 function defaultImgUrl(obj) {
-    obj.src = defaultImg+'_crop_42x42';
+    obj.src = defaultImg + '_crop_42x42';
 }
+
 function goJiapu(id) {
-    window.location.href = "editship.html?pedigreeId="+id;
+    window.location.href = "editship.html?pedigreeId=" + id;
 }
-function loadMore() {
-    getDataFromServer("/api/pedigree/list", {currentPage: page, limit: 20,content:$("input[type=search]").val()}, function (res) {
+
+function loadMore(content) {
+    getDataFromServer("/api/pedigree/list", {currentPage: page, limit: 20, content: content}, function (res) {
         if (res.code == "SUCCESS") {
             let list = res.result.content;
             if (list && list.length > 0) {
                 var record = "";
                 for (let item of list) {
-                    record += '<li class="mui-table-view-cell mui-media" onclick="goJiapu('+item.pedigreeId+')">' +
-                        '<img onerror="defaultImgUrl(this)"  class="mui-media-object mui-pull-left" src="' +imgBase+ item.totem +'_crop_42x42'+ '">' +
+                    record += '<li class="mui-table-view-cell mui-media" onclick="goJiapu(' + item.pedigreeId + ')">' +
+                        '<img onerror="defaultImgUrl(this)"  class="mui-media-object mui-pull-left" src="' + imgBase + item.totem + '_crop_42x42' + '">' +
                         '<div class="mui-media-body">' + item.nickname +
                         '<p>' + item.nickname + '</p>' +
                         '<p class="time">' + item.rowAddTime + '</p>' +
@@ -35,16 +38,19 @@ function loadMore() {
                         '</div>' +
                         '</li>';
                 }
-                $("#data").append(record);
+                if (page ==1){
+                    $("#data").html(record)
+                }else {
+                    $("#data").append(record);
+                }
                 islast = res.result.last;
                 isLoad = false;
             }
             page++;
-        }else {
+        } else {
             alert(res.message)
         }
     }, function (error) {
         console.log(JSON.stringify(error));
     });
 }
-loadMore();
